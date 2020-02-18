77babf9b5baf59672fbe15ebb8c60f615a48115a
mattchley <mattchley@gmail.com>
2020-02-18 11:20:02 -0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
-
 import Wrapper from "./components/Wrapper";
-
+import Nav from "./components/Nav"
+import SearchBox from "./components/SearchBox";
 
 class App extends Component {
   // Setting this.state.employee to the employee json array
-  state = {
-    employee
-  };
+  // state = {
+  //   employee
+  // };
 
-  
+
   render() {
     return (
-      <Wrapper>
 
+      <Wrapper>
+        <Nav>
+        </Nav>
+      <SearchBox />
       </Wrapper>
+
     );
   }
 }
---
+++ src/components/SearchBox.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function SearchBox() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <form className="form-inline my-2 my-lg-0">
+                    <input
+                        className="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Search"
+                        aria-label="Search"
+                    />
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+                        Search
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default SearchBox;
+
